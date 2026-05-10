0b7385557a4148a19a7cd2f20ad65156c77ea2b7
Fahri Haikal Ridwan <fahri.haikal.2505356@students.um.ac.id>
2026-05-10 11:24:58 +0700

    Update 2026_04_27_132539_laporan_penjualan_bulanan.php

--- database/migrations/2026_04_27_132539_laporan_penjualan_bulanan.php
+++ database/migrations/2026_04_27_132539_laporan_penjualan_bulanan.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id('laporan_id');
             $table->tinyInteger('bulan');
             $table->year('tahun');
-            $table->integer('total_transaksi')->default(0);
             $table->integer('total_sepatu_terjual')->default(0);
             $table->decimal('total_omset', 15, 2)->default(0);
             $table->unique(['bulan', 'tahun']); // Gabungan bulan & tahun harus unik
